01cc84730fab753a465125807f005ebfc0b8ecd8
Joseph Congdon <jocongdon@yahoo.com>
2022-02-03 21:26:43 -0600

    second commit

---
+++ src/main/java/com/example/springcounter/SpringCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcounter/SpringCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
